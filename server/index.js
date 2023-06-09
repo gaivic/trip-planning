2cdff1024538e4d04326f7babf4b3ce12737891f
Kailiao0727 <liaoyikai0727@gmail.com>
2023-06-09 13:38:54 +0800

    add files

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+
+dotenv.config();
+const app = express();
+
+app.use()
